704d89195514bcdab1d1bda6b8131efc0db3e22f
Julian Woo <julian.woo@student.nhlstenden.com>
2025-03-11 13:09:42 +0100

    fixed

--- Project_BattleBots/week4/pakOp/pakOp.ino
+++ Project_BattleBots/week4/pakOp/pakOp.ino
@@ -16,7 +16,7 @@ const int MOTOR_B2 = 9;   // Right motor pin 2
 const int MAX_SPEED = 255;
 const int MID_SPEED = 220;
 
-//range how the gripper open and closes. 360 degrees
+// Range how the gripper open and closes. 360 degrees
 int OPENGRIP_VALUE = 120;
 int CLOSEGRIP_VALUE = 50;
 
@@ -32,6 +32,7 @@ const long DRIVE_START_TIME = 3000;   // Start driving after 3s
 const long GRAB_TIME = 4000;         // Grab cone after 4s 
 const long HOLD_TIME = 6000;         // Stop and hold after 6s
 const long DROP_TIME = 8000;        // Drop after 8s
+const long DRIVE_AGAIN_TIME = 5000;  // Drive again after 5s
 
 boolean isGripClosed = false;
 
@@ -46,17 +47,14 @@ void setup()
 {
   pixels.begin(); // INITIALIZE NeoPixel strip object
   
-  // initialize serial communication:
+  // Initialize serial communication:
   Serial.begin(9600);
   pinMode(MOTOR_A1, OUTPUT);
   pinMode(MOTOR_A2, OUTPUT);
   pinMode(MOTOR_B1, OUTPUT);
   pinMode(MOTOR_B2, OUTPUT);
-
   pinMode(SERVO_PIN, OUTPUT);
-
 }
-
 
 void loop() 
 { 
@@ -64,7 +62,6 @@ void loop()
 }
 
 void pickUp() {
-<<<<<<< HEAD
     currentMillis = millis();
     unsigned long elapsedTime = currentMillis - previousActionMillis;
     
@@ -98,8 +95,8 @@ void pickUp() {
       moveForward();
     }
     
-    // Stop and grab the cone
-    else if(elapsedTime >= GRAB_TIME && elapsedTime < DRIVE_AGAIN_TIME && !isGripClosed) {
+    // Grab the cone
+    else if(elapsedTime >= GRAB_TIME && elapsedTime < HOLD_TIME && !isGripClosed) {
       stopMotor();
       isGripClosed = true;
     }
@@ -113,51 +110,8 @@ void pickUp() {
     else if(elapsedTime >= HOLD_TIME && elapsedTime < DROP_TIME) {
       stopMotor();
     }
-=======
-  currentMillis = millis();
-  unsigned long elapsedTime = currentMillis - previousActionMillis;
-  
-  // Keep servo active, 20 ms interval
-  if(currentMillis - previousServoMillis >= SERVO_INTERVAL) {
-    previousServoMillis = currentMillis;
-    if(isGripClosed) {
-      generatePulse(CLOSEGRIP_VALUE);
-    } else {
-      generatePulse(OPENGRIP_VALUE);
-    }
-  }
-  
-  // Open the grip
-  if(elapsedTime >= OPEN_TIME && elapsedTime < CLOSE_TIME) {
-    isGripClosed = false;
-  }
-  
-  // Close it after 1 second
-  else if(elapsedTime >= CLOSE_TIME && elapsedTime < OPEN_AGAIN_TIME) {
-    isGripClosed = true;
-  }
-  
-  // Open it again after 1 more second
-  else if(elapsedTime >= OPEN_AGAIN_TIME && elapsedTime < DRIVE_START_TIME) {
-    isGripClosed = false;
-  }
-  
-  // Drive forward to the cone
-  else if(elapsedTime >= DRIVE_START_TIME && elapsedTime < GRAB_TIME) {
-    moveForward();
-  }
-  
-  // Grab the cone
-  else if(elapsedTime >= GRAB_TIME && elapsedTime < HOLD_TIME) {
-    isGripClosed = true;
-  }
-  
-  // Stop and hold the cone
-  else if(elapsedTime >= HOLD_TIME && elapsedTime < DROP_TIME) {
-    stopMotor();
-  }
->>>>>>> 5e4fef566b9e813f99a3379e91ad0800c0d22ca1
-
+    
+    // Drop the cone after the specified time
     else if (elapsedTime >= DROP_TIME) {
       isGripClosed = false;
     }
@@ -169,10 +123,9 @@ void moveForward() {
 
   analogWrite(MOTOR_B1, MID_SPEED);  
   analogWrite(MOTOR_B2, 0);
-
 }
 
-//function to make robot stop
+// Function to make robot stop
 void stopMotor(){
   brakeLight();
   analogWrite(MOTOR_A1, 0);  
@@ -184,22 +137,21 @@ void stopMotor(){
 
 void brakeLight() {
   pixels.clear(); // Set all pixel colors to 'off'
-  pixels.setPixelColor(0, pixels.Color(0, 150, 0)); //Set left rear color to orange (G,R,B)
-  pixels.setPixelColor(1, pixels.Color(0, 150, 0)); //Set right rear color to orange (G,R,B)
+  pixels.setPixelColor(0, pixels.Color(0, 150, 0)); // Set left rear color to orange (G,R,B)
+  pixels.setPixelColor(1, pixels.Color(0, 150, 0)); // Set right rear color to orange (G,R,B)
   pixels.show();   // Send the updated pixel colors to the hardware.
 }
 
 void rightSignal() {
     pixels.clear(); // Set all pixel colors to 'off'
-    pixels.setPixelColor(1, pixels.Color(70, 255, 0)); //Set right rear color to orange (G,R,B)
-    pixels.setPixelColor(2, pixels.Color(70, 255, 0)); //Set right front color to orange (G,R,B)
+    pixels.setPixelColor(1, pixels.Color(70, 255, 0)); // Set right rear color to green (G,R,B)
+    pixels.setPixelColor(2, pixels.Color(70, 255, 0)); // Set right front color to green (G,R,B)
     pixels.show();   // Send the updated pixel colors to the hardware.
 }
 
 void leftSignal() {
     pixels.clear(); // Set all pixel colors to 'off'
-    pixels.setPixelColor(0, pixels.Color(70, 255, 0)); //Set left rear color to orange (G,R,B)
-    pixels.setPixelColor(3, pixels.Color(70, 255, 0)); //Set left front color to orange (G,R,B)
+    pixels.setPixelColor(0, pixels.Color(70, 255, 0)); // Set left rear color to green (G,R,B)
+    pixels.setPixelColor(3, pixels.Color(70, 255, 0)); // Set left front color to green (G,R,B)
     pixels.show();   // Send the updated pixel colors to the hardware.
 }
-
